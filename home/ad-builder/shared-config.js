94c51e02e4da44ac9a5b09902ef953999c87c911
campaign-craft <76563638+campaign-craft@users.noreply.github.com>
2026-03-03 13:30:16 +0700

    a

---
+++ home/ad-builder/shared-config.js
@@ -0,0 +1,394 @@
+const adSlots = {
+  "728x90_leaderboard": {
+    name: "Leaderboard 1",
+    width: 728,
+    height: 90,
+    template: "DESKTOP/template_leaderboard_728x90.html",
+  },
+  "970x90_leaderboard": {
+    name: "Leaderboard 2",
+    width: 970,
+    height: 90,
+    template: "DESKTOP/template_leaderboard_970x90.html",
+  },
+  "970x250": {
+    name: "Billboard",
+    width: 970,
+    height: 250,
+    template: "DESKTOP/template_billboard.html",
+  },
+  "300x600": {
+    name: "Giant Banner",
+    width: 300,
+    height: 600,
+    template: "DESKTOP/template_giant_banner_desktop.html",
+  },
+  "300x250_mr1": {
+    name: "MR 1",
+    width: 300,
+    height: 250,
+    template: "DESKTOP/template_mr1.html",
+  },
+  "300x250_mr2": {
+    name: "MR 2",
+    width: 300,
+    height: 250,
+    template: "DESKTOP/template_mr2.html",
+  },
+  "300x250_mr3": {
+    name: "MR 3",
+    width: 300,
+    height: 250,
+    template: "DESKTOP/template_mr3.html",
+  },
+  "1008x60": {
+    name: "Horizontal Ad",
+    width: 1008,
+    height: 60,
+    template: "DESKTOP/template_horizontal_ad_desktop_1008x60.html",
+  },
+  "1100x50": {
+    name: "Horizontal Ad",
+    width: 1100,
+    height: 50,
+    template: "DESKTOP/template_horizontal_ad_desktop_1100x50.html",
+  },
+  "120x600_skin_ad": {
+    name: "Skin Ad",
+    width: 120,
+    height: 600,
+    template: "DESKTOP/template_skin_ad.html",
+  },
+  "160x600": {
+    name: "Skyscraper",
+    width: 160,
+    height: 600,
+    template: "DESKTOP/template_skyscraper.html",
+  },
+  "300x250_baloon": {
+    name: "Baloon Ads",
+    width: 300,
+    height: 250,
+    template: "DESKTOP/template_baloon_ads.html",
+  },
+  "520x730_article_underlay": {
+    name: "Article Underlay",
+    width: 520,
+    height: 730,
+    template: "DESKTOP/template_article_underlay_ad.html",
+  },
+  "300x250_article_inside": {
+    name: "Article Inside",
+    width: 300,
+    height: 250,
+    template: "DESKTOP/template_article_inside_ad.html",
+  },
+  "320x100_below_photo": {
+    name: "Below Photo",
+    width: 320,
+    height: 100,
+    template: "DESKTOP/template_below_photo.html",
+  },
+  "320x100_below_article": {
+    name: "Below Article",
+    width: 320,
+    height: 100,
+    template: "DESKTOP/template_below_article.html",
+  },
+  "468x60_pickwords": {
+    name: "Pickwords",
+    width: 468,
+    height: 60,
+    template: "DESKTOP/template_pickwords.html",
+  },
+
+  "640x480_pushdown_desktop": {
+    name: "Push Down",
+    width: 640,
+    height: 480,
+    template: "DESKTOP/template_pushdown.html",
+  },
+  "700x393_scroll_headline_desktop": {
+    name: "Scroll Headline Desktop",
+    width: 700,
+    height: 393,
+    template: "DESKTOP/template_scroll_headline_desktop.html",
+  },
+  "320x50_top_mobile": {
+    name: "Top Banner Mobile 50",
+    width: 320,
+    height: 50,
+    template: "MOBILE/template_top_banner_mobile_320x50.html",
+  },
+  "320x100_top_mobile": {
+    name: "Top Banner Mobile 100",
+    width: 320,
+    height: 100,
+    template: "MOBILE/template_top_banner_mobile_320x100.html",
+  },
+  "320x150_top_mobile": {
+    name: "Top Banner Mobile 150",
+    width: 320,
+    height: 150,
+    template: "MOBILE/template_top_banner_mobile_320x150.html",
+  },
+  "300x250_middle_mobile": {
+    name: "Middle Banner Mobile",
+    width: 300,
+    height: 250,
+    template: "MOBILE/template_middle_banner_mobile.html",
+  },
+  "320x100_below_photo_mobile": {
+    name: "Below Photo Mobile",
+    width: 320,
+    height: 100,
+    template: "MOBILE/template_below_photo_mobile.html",
+  },
+  "320x100_below_article_mobile": {
+    name: "Below Article Mobile",
+    width: 320,
+    height: 100,
+    template: "MOBILE/template_below_article_mobile.html",
+  },
+  "320x50_horizontal_mobile": {
+    name: "Horizontal Mobile",
+    width: 320,
+    height: 50,
+    template: "MOBILE/template_horizontal_mobile_320x50.html",
+  },
+  "320x100_horizontal_mobile": {
+    name: "Horizontal Mobile",
+    width: 320,
+    height: 100,
+    template: "MOBILE/template_horizontal_mobile_320x100.html",
+  },
+  "640x940_underlay_mobile": {
+    name: "Underlay Mobile",
+    width: 640,
+    height: 940,
+    template: "MOBILE/template_underlay_mobile_640x940.html",
+  },
+  "640x500_underlay_mobile": {
+    name: "Underlay Mobile",
+    width: 640,
+    height: 500,
+    template: "MOBILE/template_underlay_mobile_640x500.html",
+  },
+  "300x250_bottom_mobile": {
+    name: "Bottom Banner Mobile",
+    width: 300,
+    height: 250,
+    template: "MOBILE/template_bottom_banner_mobile.html",
+  },
+  "300x250_article_inside_mobile": {
+    name: "Article Inside Mobile",
+    width: 300,
+    height: 250,
+    template: "MOBILE/template_article_inside_mobile.html",
+  },
+  "300x600_giant_mobile": {
+    name: "Giant Banner Mobile",
+    width: 300,
+    height: 600,
+    template: "MOBILE/template_giant_mobile.html",
+  },
+  "320x50_pickwords_mobile": {
+    name: "Pickwords Mobile",
+    width: 320,
+    height: 50,
+    template: "MOBILE/template_pickwords_mobile.html",
+  },
+  "320x480_pushdown_mobile": {
+    name: "Pushdown Mobile",
+    width: 320,
+    height: 480,
+    template: "MOBILE/template_pushdown_mobile.html",
+  },
+  "300x200_rollup_mobile": {
+    name: "Roll Up Mobile",
+    width: 300,
+    height: 200,
+    template: "MOBILE/template_rollup_mobile.html",
+  },
+  "700x393_scroll_headline_mobile": {
+    name: "Scroll Headline Mobile",
+    width: 700,
+    height: 393,
+    template: "MOBILE/template_scroll_headline_mobile.html",
+  },
+
+};
+
+const logoUrls = {
+  default: "https://asset-1.tstatic.net/img/logo/tribun/svg/tribunnews.svg",
+  aceh: "https://asset-1.tstatic.net/img/logo/daerah/svg3/serambiindonesia.svg",
+  prohaba: "https://asset-1.tstatic.net/img/logo/daerah/svg3/prohaba.svg",
+  gayo: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribungayo.svg",
+  medan: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunmedan.svg",
+  padang: "https://asset-1.tribunnews.com/img/logo/tribun/svg/TribunPadang.svg",
+  pekanbaru: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunpekanbaru.svg",
+  batam: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunbatam.svg",
+  jambi: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunjambi.svg",
+  palembang: "https://asset-1.tstatic.net/img/logo/daerah/svg3/sriwijayapost.svg",
+  bangka: "https://asset-1.tstatic.net/img/logo/daerah/svg3/bangkapos.svg",
+  belitung: "https://asset-1.tstatic.net/img/logo/daerah/svg3/posbelitung.svg",
+  bengkulu: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/Tribunbengkulu.svg",
+  lampung: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunlampung.svg",
+  sumsel: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunsumsel.svg",
+  jakarta: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunjakarta.svg",
+  wartakota: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/wartakotalive.svg",
+  banten: "https://asset-1.tstatic.net/img/logo/tribun/svg/tribunbanten2.svg",
+  tangerang: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/TribunTangerang.svg",
+  jabar: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunjabar5.svg",
+  depok: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/tribunnewsdepok.svg",
+  bekasi: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/Tribunbekasi.svg",
+  bogor: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunnewsbogor.svg",
+  priangan: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/TribunPriangan.svg",
+  cirebon: "https://asset-1.tribunnews.com/img/logo/tribun/svg/TribunCirebon.svg",
+  jateng: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunjateng.svg",
+  solo: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunsolo.svg",
+  banyumas: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/tribunbanyumas3.svg",
+  mataraman: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/tribunMataraman.svg",
+  jatim: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunjatim.svg",
+  surabaya: "https://asset-1.tstatic.net/img/logo/daerah/svg3/surya.svg",
+  suryamalang: "https://asset-1.tstatic.net/img/logo/daerah/svg3/suryamalang.svg",
+  madura: "https://asset-1.tribunnews.com/img/logo/tribun/svg/TribunMadura.svg",
+  jogja: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunjogja.svg",
+  bali: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunbali.svg",
+  pontianak: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunpontianak.svg",
+  kalteng: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunkalteng.svg",
+  kaltim: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunkaltim.svg",
+  kaltara: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunkaltara.svg",
+  banjarmasin: "https://asset-1.tstatic.net/img/logo/daerah/svg3/banjarmasinpost.svg",
+  sulbar: "https://asset-1.tribunnews.com/img/logo/daerah/sulbar/tribun-sulbar.com.png",
+  makassar: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribuntimur.svg",
+  toraja: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/TribunToraja.svg",
+  sultra: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/tribunnewssultra.svg",
+  palu: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/Tribun_Palu.svg",
+  manado: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunmanado.svg",
+  gorontalo: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/TribunGorontalo.svg",
+  lombok: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunlombok.svg",
+  kupang: "https://asset-1.tstatic.net/img/logo/daerah/svg3/poskupang.svg",
+  flores: "https://asset-1.tstatic.net/img/logo/daerah/svg3/tribunflores.svg",
+  ternate: "https://asset-1.tribunnews.com/img/logo/tribun/svg/TribunTernate.svg",
+  ambon: "https://asset-1.tribunnews.com/img/logo/tribun/svg/TribunAmbon.svg",
+  papua: "https://asset-1.tribunnews.com/img/logo/tribun/svg/TribunPapua.svg",
+  papuabarat: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/TribunPapuaBarat.svg",
+  sorong: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/TribunSorong.svg",
+  papuatengah: "https://asset-1.tribunnews.com/img/logo/daerah/svg3/TribunPapuaTengah.svg",
+};
+
+const adSlotOptionsHTML = `
+  <option value="">-- Pilih Format --</option>
+  <optgroup label="Desktop">
+    <option value="728x90_leaderboard">Leaderboard (728x90)</option>
+    <option value="970x90_leaderboard">Leaderboard (970x90)</option>
+    <option value="970x250">Billboard (970x250)</option>
+    <option value="300x600">Giant Banner (300x600)</option>
+    <option value="300x250_mr1">MR 1 (300x250)</option>
+    <option value="300x250_mr2">MR 2 (300x250)</option>
+    <option value="300x250_mr3">MR 3 (300x250)</option>
+    <option value="1008x60">Horizontal Ad (1008x60)</option>
+    <option value="1100x50">Horizontal Ad (1100x50)</option>
+    <option value="120x600_skin_ad">Skin Ad (120x600)</option>
+    <option value="520x730_article_underlay">Underlay (520x720)</option>
+    <option value="160x600">Skyscraper (160x600)</option>
+    <option value="320x100_below_photo">Below Photo (320x100)</option>
+    <option value="320x100_below_article">Below Article (320x100)</option>
+    <option value="300x250_baloon">Baloon Ads (300x250)</option>
+    <option value="300x250_article_inside">Inside Ads (300x250)</option>
+    <option value="468x60_pickwords">Pickwords (468x60)</option>
+    <option value="640x480_pushdown_desktop">Push Down (640x480)</option>
+    <option value="700x393_scroll_headline_desktop">Scroll Headline (700x393)</option>
+  </optgroup>
+  <optgroup label="Mobile">
+    <option value="320x50_top_mobile">Top Banner (320x50)</option>
+    <option value="320x100_top_mobile">Top Banner (320x100)</option>
+    <option value="320x150_top_mobile">Top Banner (320x150)</option>
+    <option value="300x250_middle_mobile">Middle Banner (300x250)</option>
+    <option value="320x100_below_photo_mobile">Below Photo (320x100)</option>
+    <option value="320x100_below_article_mobile">Below Article (320x100)</option>
+    <option value="320x50_horizontal_mobile">Horizontal Banner (320x50)</option>
+    <option value="320x100_horizontal_mobile">Horizontal Banner (320x100)</option>
+    <option value="640x940_underlay_mobile">Underlay (640x940)</option>
+    <option value="640x500_underlay_mobile">Underlay (640x500)</option>
+    <option value="300x250_bottom_mobile">Bottom Banner (300x250)</option>
+    <option value="300x250_article_inside_mobile">Article Inside (300x250)</option>
+    <option value="300x600_giant_mobile">Giant Banner (300x600)</option>
+    <option value="320x50_pickwords_mobile">Pickwords Banner (320x50)</option>
+    <option value="320x480_pushdown_mobile">Pushdown Banner (320x480)</option>
+    <option value="300x200_rollup_mobile">Roll Up Banner (300x200)</option>
+    <option value="700x393_scroll_headline_mobile">Scroll Headline (700x393)</option>
+  </optgroup>
+`;
+
+const logoOptionsHTML = `
+  <option value="default">Default (Tribunnews)</option>
+  <option value="aceh">Serambi News</option>
+  <option value="prohaba">Prohaba</option>
+  <option value="gayo">Tribun Gayo</option>
+  <option value="medan">Tribun Medan</option>
+  <option value="padang">Tribun Padang</option>
+  <option value="pekanbaru">Tribun Pekanbaru</option>
+  <option value="batam">Tribun Batam</option>
+  <option value="jambi">Tribun Jambi</option>
+  <option value="palembang">Sripoku</option>
+  <option value="bangka">Bangka Pos</option>
+  <option value="belitung">Pos Belitung</option>
+  <option value="bengkulu">Tribun Bengkulu</option>
+  <option value="lampung">Tribun Lampung</option>
+  <option value="sumsel">Tribun Sumsel</option>
+  <option value="jakarta">Tribun Jakarta</option>
+  <option value="wartakota">Warta Kota</option>
+  <option value="banten">Tribun Banten</option>
+  <option value="tangerang">Tribun Tangerang</option>
+  <option value="jabar">Tribun Jabar</option>
+  <option value="depok">Tribun Depok</option>
+  <option value="bekasi">Tribun Bekasi</option>
+  <option value="bogor">Tribunnews Bogor</option>
+  <option value="priangan">Tribun Priangan</option>
+  <option value="cirebon">Tribun Cirebon</option>
+  <option value="jateng">Tribun Jateng</option>
+  <option value="solo">Tribun Solo</option>
+  <option value="banyumas">Tribun Banyumas</option>
+  <option value="mataraman">Tribun Mataraman</option>
+  <option value="jatim">Tribun Jatim</option>
+  <option value="surabaya">Surya</option>
+  <option value="suryamalang">Surya Malang</option>
+  <option value="madura">Tribun Madura</option>
+  <option value="jogja">Tribun Jogja</option>
+  <option value="bali">Tribun Bali</option>
+  <option value="pontianak">Tribun Pontianak</option>
+  <option value="kalteng">Tribun Kalteng</option>
+  <option value="kaltim">Tribun Kaltim</option>
+  <option value="kaltara">Tribun Kaltara</option>
+  <option value="banjarmasin">Banjarmasin Post</option>
+  <option value="sulbar">Tribun Sulbar</option>
+  <option value="makassar">Tribun Timur</option>
+  <option value="toraja">Tribun Toraja</option>
+  <option value="sultra">Tribun Sultra</option>
+  <option value="palu">Tribun Palu</option>
+  <option value="manado">Tribun Manado</option>
+  <option value="gorontalo">Tribun Gorontalo</option>
+  <option value="lombok">Tribun Lombok</option>
+  <option value="kupang">Pos Kupang</option>
+  <option value="flores">Tribun Flores</option>
+  <option value="ternate">Tribun Ternate</option>
+  <option value="ambon">Tribun Ambon</option>
+  <option value="papua">Tribun Papua</option>
+  <option value="papuabarat">Tribun Papua Barat</option>
+  <option value="sorong">Tribun Sorong</option>
+  <option value="papuatengah">Tribun Papua Tengah</option>
+`;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const adSlotSelect = document.getElementById("adSlotSelect");
+  if (adSlotSelect && adSlotSelect.options.length <= 1) {
+    adSlotSelect.innerHTML = adSlotOptionsHTML;
+  }
+
+  const logoSelect = document.getElementById("logoSelect");
+  if (logoSelect && logoSelect.options.length <= 1) {
+    logoSelect.innerHTML = logoOptionsHTML;
+  }
+});
